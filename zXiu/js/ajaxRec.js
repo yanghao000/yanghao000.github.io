ee12c6f5853fe387a8eee4ff8d040ee49cb2b24c
yanghao000 <472994534@qq.com>
2016-04-02 10:36:38 +0800

    123
    123

--- zXiu/js/ajaxRec.js
+++ zXiu/js/ajaxRec.js
@@ -1,16 +1,18 @@
-//var Rec = function(){}
-//Rec.prototype = {
-//	getRec:function(url,callback){
-//		var xhr = new XMLHttpRequest();
-//		xhr.open("get",url,true);
-//		xhr.onreadystatechange = function(){
-//			callback(xhr.responseText);
-//		}
-//		xhr.send();
-//	},
-//	postRec:function(){}
-//}
-//var rec = new Rec();
-//rec.getRec("http://datainfo.duapp.com/shopdata/getGoods.php",function(data){
-//	console.log(data);
-//});
+var Rec = function(){}
+Rec.prototype = {
+	getRec:function(url,callback){
+		var xhr = new XMLHttpRequest();
+		xhr.open("get",url,true);
+		xhr.send();
+		xhr.onreadystatechange = function(){
+			if(req.readyState == 4){
+			alert(req.responseText);
+		}
+	},
+	postRec:function(){}
+}
+var rec = new Rec();
+rec.getRec("http://datainfo.duapp.com/shopdata/getGoods.php",function(data){
+	console.log(data);
+	var json = JSON.parse(data);
+});
